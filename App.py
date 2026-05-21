3f77204083fa060492bf775b0f839aecafb5c67c
itzme-yml <lopesyasmin.yuri@gmail.com>
2026-05-21 16:34:24 -0300

    Update App.py

--- App.py
+++ App.py
@@ -14,3 +14,31 @@ st.markdown('---')
 
 dias = st.text_input(f'Por quantos dias o {option} foi alugado?')
 km = st.text_input(f'Quantos km você rodou com o {option}?')
+
+if option == 'BMW' :
+     diaria = 450
+
+elif option == 'Mustang' :
+    diaria = 500
+
+elif option == 'Porche' :
+    diaria = 300
+
+elif option == 'Fusca' :
+    diaria = 250
+
+elif option == 'Toro' :
+    diaria = 550
+
+
+
+
+if st.button('Calcular'):
+    dias = int(dias)
+    km = float(km)
+
+    total_dias = dias *diaria
+    total_km = km * 0.15
+    aluguel_total = total_dias+total_km
+
+    st.warning(f'você alugou o {option} por {dias} dias e rodou {km} km. O valor total a pagar é R${aluguel_total:2f}')
